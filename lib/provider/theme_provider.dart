4d9a66aff292e53f644657008f3ec1ebf207a37e
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-17 18:48:10 +0600

    Light mode color added

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/theme/dark_mode.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
-StateProvider themeProvider = StateProvider((ref)=> )
+StateProvider themeProvider = StateProvider((ref) => darkMode);
