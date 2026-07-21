b3a9f2a9ad5946403475774e18ff10b563ae5030
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-21 23:10:52 +0600

    My app entry point separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,12 @@
-import 'package:ecommerce/utils/theme/theme.dart';
+import 'package:ecommerce/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//entry point on the app
 void main() {
   //todo stuff
 
-  runApp(ProviderScope(child: const MyApp()));
-}
-
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      home: Scaffold(),
-    );
-  }
+  runApp(ProviderScope(child: const App()));
 }
 
 //mytodo list:
