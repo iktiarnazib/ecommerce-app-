3c9377772add344eef6ea28bc05033bc9828ce34
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-21 23:17:08 +0600

    Primary color added

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 class TColors {
   TColors._();
+
+  //0XFF is to convert it to hexa decimal, the 4B68FF is the actual color
+  static const Color primaryColor = Color(0xFF4B68FF);
 }
