30894240284d303aea2a0d463cf854761783deb9
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-23 00:25:59 +0600

    Image string add

--- lib/utils/constants/image_string.dart
+++ lib/utils/constants/image_string.dart
@@ -1,3 +1,11 @@
 class TImage {
-  TImage._();
+  // -- App Logos
+  static const String darkAppLogo =
+      "assets/icons/t-store-splash-logo-black.png";
+  static const String lightAppLogo =
+      "assets/icons/t-store-splash-logo-light.png";
+
+  // -- Social Logos
+  static const String google = "assets/images/google-logo.png";
+  static const String facebook = "assets/images/facebook-logo.png";
 }
