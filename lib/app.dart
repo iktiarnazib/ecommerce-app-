5f4eceb6c4542cb1685d1e6b912b2c05c24ceba8
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-22 23:32:37 +0600

    My app updated

--- lib/app.dart
+++ lib/app.dart
@@ -2,8 +2,8 @@ import 'package:ecommerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+class App extends ConsumerWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
