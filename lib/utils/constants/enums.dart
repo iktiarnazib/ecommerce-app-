7a452740e7bf88142edcc052872bf8c827936c93
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-23 22:24:54 +0600

    Enums list of textsizes, orderstatus, paymentmethod

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,15 @@
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethod {
+  payment,
+  gogglePay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  paystack,
+  razorpay,
+  paytm,
+}
--- lib/utils/constants/text_string.dart
+++ lib/utils/constants/text_string.dart
@@ -5,13 +5,13 @@ class TText {
   static const String onBoardingTitle3 = "Deliver at your door step";
 
   static const String onBoardingSubTitle1 =
-      "Welcome to a World of Limitless Choices";
+      "Welcome to a World of LimitLess Choices - Your Perfect Product Awaits!";
   static const String onBoardingSubTitle2 =
-      "For Seamless Transactions, Choose Your";
+      "For SeamLess Transactions, Choose Your Payment Path - Your Convenience, Our Priority";
   static const String onBoardingSubTitle3 =
-      "From Our Doorstep to Yours - Swift,";
+      "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
 
   // -- Home
   static const String homeAppbarTitle = "Good day for shopping";
-  static const String homeAppbarSubTitle = "Taimoor Sikander";
+  static const String homeAppbarSubTitle = "Taimoon Sikander";
 }
