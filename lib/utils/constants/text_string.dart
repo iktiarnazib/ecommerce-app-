a8b06814ffc6902626b989fc67117ad75d6fbcb5
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-23 22:14:54 +0600

    Const text added

---
+++ lib/utils/constants/text_string.dart
@@ -0,0 +1,17 @@
+class TText {
+  // -- OnBoarding Texts
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  static const String onBoardingSubTitle1 =
+      "Welcome to a World of Limitless Choices";
+  static const String onBoardingSubTitle2 =
+      "For Seamless Transactions, Choose Your";
+  static const String onBoardingSubTitle3 =
+      "From Our Doorstep to Yours - Swift,";
+
+  // -- Home
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "Taimoor Sikander";
+}
