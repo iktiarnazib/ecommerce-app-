86ff6d4aeb5524f3cfe947589bf6aea5c28f77cc
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-24 19:36:38 +0600

    Email validator method adde

--- lib/utils/validators/validator.dart
+++ lib/utils/validators/validator.dart
@@ -3,5 +3,14 @@ class TValidator {
     if (email == null || email.isEmpty) {
       return 'Email is required';
     }
+    //regular expression or email validation
+    // Regular expression for email validation
+    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    if (!emailRegExp.hasMatch(email)) {
+      return 'Invalid email address';
+    }
+
+    return null;
   }
 }
