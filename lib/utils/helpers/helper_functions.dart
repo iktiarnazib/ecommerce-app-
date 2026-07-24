fd09096129b04bcea1574ef28752d419deaa126d
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-24 15:28:48 +0600

    Helper function added

--- lib/utils/helpers/helper_functions.dart
+++ lib/utils/helpers/helper_functions.dart
@@ -26,4 +26,28 @@ class THelperFunctions {
 
     return null;
   }
+
+  static void showSnackBar(String message, BuildContext context) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  static void showAlert(String title, String message, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            ), // TextButton
+          ],
+        ); // AlertDialog
+      },
+    );
+  }
 }
