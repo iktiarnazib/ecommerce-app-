ed41fbadc8b1d375998c5dcaa10fa748f6cfc3ea
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-27 21:45:20 +0600

    signUpPageFirstNameLastName

--- lib/features/authentication/screens/signup/signup_screen.dart
+++ lib/features/authentication/screens/signup/signup_screen.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/constants/text_string.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -10,6 +13,54 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //*Title
+                Text(
+                  TText.signupTitle,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: TSizes.spaceBtwSections),
+                //* Form
+                Form(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: TText.firstName,
+                                prefixIcon: Icon(Iconsax.user),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: TSizes.sm),
+                          Expanded(
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: TText.lastName,
+                                prefixIcon: Icon(Iconsax.user),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
