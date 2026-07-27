64567a0f12df86ca90ed0f6dd88eeb40b7162d67
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-27 14:18:47 +0600

    Form creation done

--- lib/features/authentication/screens/login/login_screen.dart
+++ lib/features/authentication/screens/login/login_screen.dart
@@ -51,6 +51,54 @@ class LoginScreen extends StatelessWidget {
                   TextFormField(
                     decoration: InputDecoration(
                       prefixIcon: Icon(Iconsax.direct_right),
+                      labelText: TText.email,
+                    ),
+                  ),
+                  SizedBox(height: TSizes.spaceBtwInputFields),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Iconsax.password_check),
+                      labelText: TText.password,
+                      suffixIcon: Icon(Iconsax.eye_slash),
+                    ),
+                  ),
+                  SizedBox(height: TSizes.spaceBtwInputFields / 2),
+
+                  //* remember me & forget password
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //*remember me
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Checkbox(value: true, onChanged: (value) {}),
+                            Text(TText.rememberMe),
+                          ],
+                        ),
+                      ),
+
+                      //* forgot password
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(TText.forgetPassword),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(TText.signIn),
+                    ),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text(TText.createAccount),
                     ),
                   ),
                 ],
