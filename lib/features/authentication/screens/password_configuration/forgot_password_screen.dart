bbb5938ca34d396c1a36671c9c4ff433557a7045
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-28 20:42:28 +0600

    Password reset screen

---
+++ lib/features/authentication/screens/password_configuration/forgot_password_screen.dart
@@ -0,0 +1,22 @@
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ListView(children: []),
+      ),
+    );
+  }
+}
