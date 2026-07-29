968d8405cffaec39165fd08bf40f40b5542b65e7
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-30 00:35:28 +0600

    Home screen updated

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,10 +8,33 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [Column(children: [
-
-          ],
-        )]),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                color: TColors.primary,
+                padding: EdgeInsets.all(0),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 400,
+                      width: 400,
+                      padding: const EdgeInsets.all(0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(400),
+                        color: TColors.textWhite.withAlpha(
+                          25,
+                        ), //10% almost, it stores total 8 bit binary for color, which is max 255 steps
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
