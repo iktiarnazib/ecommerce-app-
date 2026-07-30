571d6cb8f1e5f2a7a21d5b9dd5673447784dc1dc
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-30 13:09:50 +0600

    Circular container updated

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:ecommerce/features/shop/screens/home/widgets/container_widget.dart';
+import 'package:ecommerce/features/shop/screens/home/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:ecommerce/utils/constants/colors.dart';
-import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,12 +15,35 @@ class HomeScreen extends StatelessWidget {
               Container(
                 color: TColors.primary,
                 padding: EdgeInsets.all(0),
-                child: Stack(
-                  children: [
-                    TCircularContainer(
-                      backgroundColor: TColors.white.withAlpha(25),
-                    ),
-                  ],
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -150,
+                        right: -250,
+                        child: TCircularContainer(
+                          backgroundColor: TColors.white.withAlpha(25),
+                        ),
+                      ),
+                      Positioned(
+                        height: 150,
+                        width: -300,
+                        child: TCircularContainer(
+                          backgroundColor: TColors.white.withAlpha(25),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          //appbar
+
+                          //search bar
+
+                          //catagories
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
