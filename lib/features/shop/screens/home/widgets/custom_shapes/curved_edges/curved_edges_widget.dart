fa14f01a548b689fe674168dab598769a93f918f
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-30 22:24:03 +0600

    Home screen container connected

---
+++ lib/features/shop/screens/home/widgets/custom_shapes/curved_edges/curved_edges_widget.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce/features/shop/screens/home/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:flutter/material.dart';
+
+class TCurvedEdgesWidget extends StatelessWidget {
+  final Widget? child;
+  const TCurvedEdgesWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(clipper: TCurvedEdges(), child: child);
+  }
+}
