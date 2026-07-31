dadef38b8819bf36811827759f762467bf484127
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-31 20:02:15 +0600

    Modular appbar added

---
+++ lib/features/shop/screens/home/widgets/home_app_bar.dart
@@ -0,0 +1,38 @@
+import 'package:ecommerce/common/widgets/appbar/appbar.dart';
+import 'package:ecommerce/common/widgets/product_cart/cart_product_icon.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/text_string.dart';
+import 'package:flutter/material.dart';
+
+class THomeAppbar extends StatelessWidget {
+  const THomeAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TAppBar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            TText.homeAppbarTitle,
+            style: Theme.of(
+              context,
+            ).textTheme.labelMedium!.apply(color: TColors.grey),
+          ),
+          Text(
+            TText.homeAppbarSubTitle,
+            style: Theme.of(
+              context,
+            ).textTheme.headlineSmall!.apply(color: TColors.white),
+          ),
+        ],
+      ),
+      actions: [
+        TCartCounterIcon(
+          iconColor: TColors.white.withAlpha(200),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
