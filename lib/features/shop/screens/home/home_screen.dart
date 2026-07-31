965df6241dccbdcbad00c8297b143a269941e39f
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-31 21:48:09 +0600

    Modular section heading

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:ecommerce/features/shop/screens/home/widgets/home_app_bar.dart';
-import 'package:ecommerce/features/shop/screens/home/widgets/search_bar.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:ecommerce/utils/constants/text_string.dart';
 import 'package:flutter/material.dart';
@@ -27,13 +28,65 @@ class HomeScreen extends StatelessWidget {
                   icon: Iconsax.search_normal,
                   text: TText.searchForItems,
                 ),
+                const SizedBox(height: TSizes.spaceBtwSections),
 
                 //*Catagories
+                TSectionHeading(),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class TSectionHeading extends StatelessWidget {
+  final Color? textColor;
+  final bool showActionButton;
+  final String title, buttonTitle;
+  final void Function()? onPressed;
+  const TSectionHeading({
+    super.key,
+    this.textColor,
+    this.showActionButton = false,
+    required this.title,
+    this.buttonTitle = "",
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: Theme.of(
+                  context,
+                ).textTheme.headlineSmall!.apply(color: textColor),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              if (showActionButton)
+                TextButton(
+                  onPressed: onPressed,
+                  child: Text(
+                    buttonTitle,
+                    style: Theme.of(
+                      context,
+                    ).textTheme.labelSmall!.apply(color: textColor),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/utils/constants/text_string.dart
+++ lib/utils/constants/text_string.dart
@@ -15,6 +15,7 @@ class TText {
   static const String homeAppbarTitle = "Good day for shopping";
   static const String homeAppbarSubTitle = "Taimoon Sikander";
   static const String searchForItems = "Search in Store";
+  static const String popularCatagories = "Popular Catagories";
 
   // -- Login
   static const String loginTitle = "Welcome Back";
