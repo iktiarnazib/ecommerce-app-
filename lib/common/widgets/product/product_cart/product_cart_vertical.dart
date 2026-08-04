1dbd597d6638389d4d9d79a9f8d0a51fd81e3022
Iktiar Nazib <iktiarnazib@gmail.com>
2026-08-04 21:42:14 +0600

    Product Cart Vertical added

---
+++ lib/common/widgets/product/product_cart/product_cart_vertical.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TProductCartVertical extends StatelessWidget {
+  const TProductCartVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(decoration: BoxDecoration());
+  }
+}
