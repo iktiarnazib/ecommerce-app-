bce1041886b22720ed8a907b3b401ccefc3b040a
Iktiar Nazib <iktiarnazib@gmail.com>
2026-08-05 21:29:33 +0600

    Todo list v15

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
   runApp(ProviderScope(child: const App()));
 }
 
-//* mytodo listv14:
-//* continue => https://www.youtube.com/watch?v=hkExmbmsCvQ&list=PL5jb9EteFAOAusKTSuJ5eRl1BapQmMDT6&index=15 (minute: 20:00)
+//* mytodo listv15:
+//* continue => https://www.youtube.com/watch?v=JsPMdnkiav8&list=PL5jb9EteFAOAusKTSuJ5eRl1BapQmMDT6&index=16 (minute: 08:30)
 //* shoot gig video for fiverr
 //* finish another tutorial
